06a5b6c01c228c5360ba1252bb6c623cae61d523
Admin <admin@Admins-MacBook-Pro.local>
2017-09-12 19:00:21 +0700

    Initial Commit

---
+++ ChineseChess/AppDelegate.h
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.h
+//  ChineseChess
+//
+//  Created by Admin on 9/12/17.
+//  Copyright © 2017 Admin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <CoreData/CoreData.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong) NSPersistentContainer *persistentContainer;
+
+- (void)saveContext;
+
+
+@end
+
---
+++ ChineseChess/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ChineseChess
+//
+//  Created by Admin on 9/12/17.
+//  Copyright © 2017 Admin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
